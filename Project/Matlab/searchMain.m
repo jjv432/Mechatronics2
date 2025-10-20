e7ecb71a181ef240bdf2cc56a8adad1fa65f8978
jjv432 <jackvranicar43@gmail.com>
2025-10-20 18:56:44 -0400

    Arms run side by side but not simultaneous

--- Project/Matlab/searchMain.m
+++ Project/Matlab/searchMain.m
@@ -18,20 +18,27 @@ searchParams.animateBool = 0;
 lh_grip = gripper('basePosition', [-3.5 2.5], 'parallelLinkLength', 1, 'endEffectorLength', 4);
 rh_grip = gripper('handedness', 'r', 'basePosition', [4 2.5], 'parallelLinkLength', 1, 'endEffectorLength', 4);
 
+%%
 % rh_grip.plotPosition(pi/2);
 % lh_grip.plotPosition(pi/2);
 
-shapes = ["circle", "square", "sine"];
-figure();
-sgtitle("Detection of Obstacle Shape");
-for i = 1:numel(shapes)
+% shapes = ["circle", "square", "sine"];
+% figure();
+% sgtitle("Detection of Obstacle Shape");
+% for i = 1:numel(shapes)
+% 
+%     obst = obstacle("shape", shapes(i));
+%     subplot(2, 2, i)
+%     hold on
+%     rh_grip.predictObstacle(obst, searchParams)
+%     lh_grip.predictObstacle(obst, searchParams)
+%     obst.drawObstacle;
+%     hold off
+%     axis padded
+% end
 
-    obst = obstacle("shape", shapes(i));
-    subplot(2, 2, i)
-    hold on
-    rh_grip.predictObstacle(obst, searchParams)
-    lh_grip.predictObstacle(obst, searchParams)
-    obst.drawObstacle;
-    hold off
-    axis padded
-end
+%% Simulataneous Grip
+obst = obstacle("shape", "square");
+searchParams.animateBool = 1;
+hand = gripperSet(lh_grip, rh_grip);
+hand.detectObstacle(obst, searchParams);
