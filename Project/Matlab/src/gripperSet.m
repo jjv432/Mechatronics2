168f3e07ce426b14030f2cc50e35c885e538b15a
jjv432 <jackvranicar43@gmail.com>
2025-10-22 13:20:15 -0400

    Cleaning up, commenting

--- Project/Matlab/src/gripperSet.m
+++ Project/Matlab/src/gripperSet.m
@@ -7,6 +7,7 @@ classdef gripperSet
     end
 
     methods
+        %** Constructor
         function obj = gripperSet(lhs, rhs,searchParams)
             obj.lhs = lhs;
             obj.rhs = rhs;
@@ -14,10 +15,13 @@ classdef gripperSet
                 obj.sP = searchParams;
             end
         end
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %****** 'Main' Functions for Obstacle Detection ******
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+        %** Search for contact with the obstacle at a given height
         function [contactPosition_lhs, contactPosition_rhs] = searchHeight(obj, obstacle, height)
-
-            %** Search for contact with the obstacle at a given height
 
             obstacle.drawObstacle();
 
@@ -59,6 +63,80 @@ classdef gripperSet
             end
         end
 
+        
+        %** Search for contact points with the obstacle at a RANGE of
+        %heights
+        function [contactPositions_lhs, contactPositions_rhs] = detectObstacle(obj, obstacle)
+
+            searchYs = obj.sP.startY:-obj.sP.dy:obj.sP.endY;
+
+            contactPositions_lhs = [];
+            contactPositions_rhs = [];
+
+            if obj.sP.saveBool
+                writerObj = VideoWriter('animation.mp4');
+                writerObj.FrameRate = 30;
+                open(writerObj);
+            end
+
+            for i = 1:numel(searchYs)
+                [l, r] = searchHeight(obj, obstacle,searchYs(i));
+                contactPositions_lhs = [contactPositions_lhs; l];
+                contactPositions_rhs = [contactPositions_rhs; r];
+            end
+
+        end
+
+
+        %** Take the search data obtained from probing the whole obstacle
+        % and plot the points where contact was made
+        function predictObstacle(obj, obstacle)
+            [endEffectorPositions_lhs, endEffectorPositions_rhs] = obj.detectObstacle(obstacle);
+
+            x_vals_lhs = endEffectorPositions_lhs(:, 1);
+            y_vals_lhs = endEffectorPositions_lhs(:, 2);
+            x_vals_rhs = endEffectorPositions_rhs(:, 1);
+            y_vals_rhs = endEffectorPositions_rhs(:, 2);
+
+            gca;
+            hold on
+            scatter(x_vals_lhs, y_vals_lhs, 'x', 'b');
+            scatter(x_vals_rhs, y_vals_rhs, 'x', 'r');
+            axis equal
+        end
+
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %****** 'Helper' Functions ******
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+
+        %** detect contact between the hand and the obstacle
+        function [contact, contact_theta, contactPosition] = detectContact(obj, theta, hand, obstacle)
+            
+            contact = 0;
+            contact_theta = [];
+            contactPosition = [];
+
+            if hand == 'r'
+                [positions,tip] = obj.rhs.definePositions(theta);
+            elseif hand == 'l'
+                [positions,tip] = obj.lhs.definePositions(theta);
+            end
+
+            % Check for collisions and return contact, theta, and position
+            % if so
+            endEffectorPosition = positions(end, :);
+            [in, on] = inpolygon(tip(1,:), tip(2,:), obstacle.coords(1,:), obstacle.coords(2,:));
+            if sum([in, on]) > 0
+                contact= 1;
+                contact_theta = theta;
+                contactPosition = endEffectorPosition;
+            end
+
+        end
+
+        %** Plot the grippers, keeping them steady if in contact
         function animateSet(obj, cur_theta, contact_lhs, contact_rhs, contact_lhs_theta, contact_rhs_theta)
             if ~contact_lhs
                 h_lhs = obj.lhs.plotPosition(cur_theta);
@@ -81,65 +159,5 @@ classdef gripperSet
         end
 
 
-        function [contact, contact_theta, contactPosition] = detectContact(obj, theta, hand, obstacle)
-            %** detect contact between the hand and the obstacle
-
-            contact = 0;
-            contact_theta = [];
-            contactPosition = [];
-
-            if hand == 'r'
-                [positions,tip] = obj.rhs.definePositions(theta);
-            elseif hand == 'l'
-                [positions,tip] = obj.lhs.definePositions(theta);
-            end
-
-            % Check for collisions and return contact, theta, and position
-            % if so
-            endEffectorPosition = positions(end, :);
-            [in, on] = inpolygon(tip(1,:), tip(2,:), obstacle.coords(1,:), obstacle.coords(2,:));
-            if sum([in, on]) > 0
-                contact= 1;
-                contact_theta = theta;
-                contactPosition = endEffectorPosition;
-            end
-
-        end
-        function [contactPositions_lhs, contactPositions_rhs] = detectObstacle(obj, obstacle)
-            %** Detect the entirety of an object
-
-            searchYs = obj.sP.startY:-obj.sP.dy:obj.sP.endY;
-
-            contactPositions_lhs = [];
-            contactPositions_rhs = [];
-
-            if obj.sP.saveBool
-                writerObj = VideoWriter('animation.mp4');
-                writerObj.FrameRate = 30;
-                open(writerObj);
-            end
-
-            for i = 1:numel(searchYs)
-                [l, r] = searchHeight(obj, obstacle,searchYs(i));
-                contactPositions_lhs = [contactPositions_lhs; l];
-                contactPositions_rhs = [contactPositions_rhs; r];
-            end
-
-        end
-
-        function predictObstacle(obj, obstacle)
-            [endEffectorPositions_lhs, endEffectorPositions_rhs] = obj.detectObstacle(obstacle);
-
-            x_vals_lhs = endEffectorPositions_lhs(:, 1);
-            y_vals_lhs = endEffectorPositions_lhs(:, 2);
-            x_vals_rhs = endEffectorPositions_rhs(:, 1);
-            y_vals_rhs = endEffectorPositions_rhs(:, 2);
-
-            gca;
-            hold on
-            scatter(x_vals_lhs, y_vals_lhs, 'x', 'b');
-            scatter(x_vals_rhs, y_vals_rhs, 'x', 'r');
-            axis equal
-        end
     end
 end
