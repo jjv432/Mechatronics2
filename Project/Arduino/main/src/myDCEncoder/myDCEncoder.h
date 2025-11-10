daa3fb2ca9ddf43f638e55db9b870513e58ed3d8
jjv432 <jackvranicar43@gmail.com>
2025-11-10 10:59:01 -0500

    Fixing header

--- Project/Arduino/main/src/myDCEncoder/myDCEncoder.h
+++ Project/Arduino/main/src/myDCEncoder/myDCEncoder.h
@@ -8,6 +8,8 @@ class myStepper
   public:
     myStepper(int direction_pin, int ch_a, int ch_b, int ch_c, int ch_d);
     void init();
+    void driveMotor(int PWM);
+    void PID(int desPos);
     void updateCurState(int channelNumber);
     int _curPos = 0; // in "ticks"
     int _curOmega = 0; // in deg/s
@@ -15,6 +17,10 @@ class myStepper
     float _ki = 1.0;
     float _kd = 1.0;
   private:
+    void ISR_0();
+    void ISR_1();
+    void ISR_2();
+    void ISR_3();
     int _IN_1;
     int _IN_2;
     int _EN;
