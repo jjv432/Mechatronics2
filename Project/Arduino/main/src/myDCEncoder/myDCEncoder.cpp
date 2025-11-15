5378096c753106ce63e8bdb1abb41e3d82b44154
jjv432 <jackvranicar43@gmail.com>
2025-11-15 14:27:41 -0500

    Untested hall effect code

--- Project/Arduino/main/src/myDCEncoder/myDCEncoder.cpp
+++ Project/Arduino/main/src/myDCEncoder/myDCEncoder.cpp
@@ -1,199 +1,257 @@
-# include "Arduino.h"
-# include "myDCEncoder.h"
+#include "myDCEncoder.h"
+#include "Arduino.h"
 
-myDCEncoder::myDCEncoder(int IN_1, int IN_2, int EN, int ch_a, int ch_b)
-{
-	_IN_1 = IN_1;
-	_IN_2 = IN_2;
-	_EN = EN;
+/////////////////////////////////////////////////
+/////////////// CONSTRUCTOR, INIT////////////////
+/////////////////////////////////////////////////
 
-	_input_ch[0] = ch_a;
-	_input_ch[1] = ch_b;
+myDCEncoder::myDCEncoder(int IN_1, int IN_2, int EN, int ch_a, int ch_b,
+                         int hallEffectPin) {
+  _IN_1 = IN_1;
+  _IN_2 = IN_2;
+  _EN = EN;
 
+  _input_ch[0] = ch_a;
+  _input_ch[1] = ch_b;
+
+  _hallEffectPin = hallEffectPin;
 }
 
 void myDCEncoder::init() {
-	// Make IN1 and IN2 output pins
-	pinMode(_IN_1, OUTPUT);
-	pinMode(_IN_2, OUTPUT);
+  // Make IN1 and IN2 output pins
+  pinMode(_IN_1, OUTPUT);
+  pinMode(_IN_2, OUTPUT);
 
-	// Make all the encoder pins inputs
-	for (int i = 0; i < 2; i++) {
+  // Make all the encoder pins inputs
+  for (int i = 0; i < 2; i++) {
     pinMode(_input_ch[i], INPUT_PULLUP);
-	}
+  }
+
+  // enable the hall effect input
+  pinMode(_hallEffectPin, INPUT);
 }
 
-void myDCEncoder::driveMotor(int PWM){
+/////////////////////////////////////////////////
+////////////// CONTROL  METHODS /////////////////
+/////////////////////////////////////////////////
 
-	// Deal with switching directions
-	if (PWM > 0){
-		digitalWrite(_IN_1, HIGH);
-		digitalWrite(_IN_2, LOW);
-	}
-	else if (PWM <= 0){
-		digitalWrite(_IN_1, LOW);
-		digitalWrite(_IN_2, HIGH);
-	}
+void myDCEncoder::PID(int desPos) {
+  // desPos will be determined as a function of compression when using the hall
+  // effect. It's useful to have a baseline PID function, though REMEMBER CURPOS
+  // IS IN "TICKS"
+  static int last_p_error = 0;
+  static unsigned int last_time = millis();
+  static int i_error = 0; // integral of error;
+  static int stationaryCounter = 0;
+  static int lastStationaryPosition = 0;
 
-	// Set the speed
-	analogWrite(_EN, abs(PWM));
-}
-void myDCEncoder::PID(int desPos){
-	// desPos will be determined as a function of compression when using the hall effect. It's useful to have a baseline PID function, though
-	// REMEMBER CURPOS IS IN "TICKS"
-	static  int last_p_error = 0;
-	static unsigned int last_time = millis();
-	static float i_error = 0; // integral of error
-	static int stationaryCounter = 0;
-	static int lastStationaryPosition = 0;
+  // get timing info
+  unsigned int now = millis();
+  unsigned int dt = now - last_time;
+  last_time = now;
 
-	// get timing info
-	unsigned int now = millis();
-	unsigned int dt = now - last_time;
-	last_time = now;
+  // find errors
+  int p_error = desPos - _curPos;                // P;
+  float d_error = (last_p_error - p_error) / dt; // D;
+  i_error += -p_error * dt;                      // I;
 
-	// find errors
-	int p_error = desPos - _curPos;							// P
-	float d_error = (last_p_error - p_error) / dt;			// D
-	i_error += -p_error * dt / 1000.;		 				// I
+  // reset for next time
+  last_p_error = p_error;
 
-	// reset for next time
-	last_p_error = p_error;	
+  // Calculate commanded PWM (speed)
+  float PwmCommand = _kp * p_error + _kd * d_error + _ki * i_error;
 
-	// Calculate commanded PWM (speed)
-	float PwmCommand = _kp * p_error + _kd * d_error + _ki * i_error;
+  if (PwmCommand > 255) {
+    PwmCommand = 255;
+  } else if (PwmCommand < -255) {
+    PwmCommand = -255;
+  }
 
-	if (PwmCommand > 255){
-		PwmCommand = 255;
-	}
-	else if (PwmCommand < -255){
-		PwmCommand = -255;
-	}
-	
-	// increment if you've been stationary
-	if (abs(p_error) <= 1){
-		stationaryCounter ++;
-	}
-	else{
-		stationaryCounter = 0;
-	}
+  // increment if you've been stationary
+  if (abs(p_error) <= 1) {
+    stationaryCounter++;
+  } else {
+    stationaryCounter = 0;
+  }
 
-	// flush integral error if you've been stationary for a while
-	if (stationaryCounter >= 15){
-		i_error = 0;
-		lastStationaryPosition = desPos;
-		// in case desPos changes, reset all of this logic
-		if (desPos != lastStationaryPosition){
-			stationaryCounter = 0;
-		}
-	}
+  // flush integral error if you've been stationary for a while
+  if (stationaryCounter >= 15) {
+    i_error = 0;
+    lastStationaryPosition = desPos;
+    // in case desPos changes, reset all of this logic
+    if (desPos != lastStationaryPosition) {
+      stationaryCounter = 0;
+    }
+  }
 
-	myDCEncoder::driveMotor(PwmCommand);
-
+  myDCEncoder::driveMotor(PwmCommand);
 }
 
-void myDCEncoder::ticksToDeg(){
-
+/////////////////////////////////////////////////
+/////////// HALL EFFECT  METHODS ////////////////
+/////////////////////////////////////////////////
+void myDCEncoder::readHallEffect(){
+  _hallEffectReadingV = analogRead(hallPin0);
+  _hallEffectReadingG = (_hallEffectReading * _hallEffectResolution - _zeroField) / _hallEffectSensitivity;  // in Gauss;
 }
 
+
+
+/////////////////////////////////////////////////
+/////////////// MOTOR METHODS  //////////////////
+/////////////////////////////////////////////////
+
+void myDCEncoder::driveMotor(int PWM) {
+
+  // Deal with switching directions
+  if (PWM > 0) {
+    digitalWrite(_IN_1, HIGH);
+    digitalWrite(_IN_2, LOW);
+  } else if (PWM <= 0) {
+    digitalWrite(_IN_1, LOW);
+    digitalWrite(_IN_2, HIGH);
+  }
+
+  // Set the speed
+  analogWrite(_EN, abs(PWM));
+}
+
+void myDCEncoder::stopMotor(bool stop) {
+  if (stop) {
+    digitalWrite(_IN_1, HIGH);
+    digitalWrite(_IN_2, HIGH);
+  }
+  analogWrite(_EN, 255);
+}
+
+void myDCEncoder::releaseMotor(bool release) {
+  if (release) {
+    digitalWrite(_IN_1, LOW);
+    digitalWrite(_IN_2, LOW);
+  }
+  analogWrite(_EN, 0);
+}
+
+void myDCEncoder::calibrateMotor(bool start) {
+
+  if (start) {
+    // let the linkage settle into zero position
+    myDCEncoder::releaseMotor(true);
+    delay(1000);
+
+    // drive the motor a little bit to get it swinging
+    startTime = millis();
+    while (millis() - startTime < 1000) {
+      myDCEncoder::driveMotor(127);
+    }
+
+    // stop the motor, and let it settle again
+    myDCEncoder::releaseMotor(true);
+    delay(1000);
+
+    // set curPos to zero now that it has settled
+    _curPos = 0;
+    _calibrated = true;
+  }
+}
+
+/////////////////////////////////////////////////
+/////////////// ENCODER METHODS /////////////////
+/////////////////////////////////////////////////
 
 // Function called by the ISRs to update the current position of the motor
-void myDCEncoder::updateCurState(){
-	// an interrupt in the main script will call this. Based on the last channel that was interrupted, you'll know which direction you spun
-	
-	// read both channels of the encoder
-	bool a = digitalRead(_input_ch[0]);
-	bool b = digitalRead(_input_ch[1]);
+void myDCEncoder::updateCurState() {
+  // an interrupt in the main script will call this. Based on the last channel
+  // that was interrupted, you'll know which direction you spun
 
-	// update state info
-	_lastState = _curState;
+  // read both channels of the encoder
+  bool a = digitalRead(_input_ch[0]);
+  bool b = digitalRead(_input_ch[1]);
 
-	// logic based on behavior of the enoder. See spec sheet 
-	if (!a&&!b){
-		_curState = 0;
-	}
-	else if(a&&!b){
-		_curState = 1;
-	}
-	else if(a&&b){
-		_curState = 2;
-	}
-	else if(!a&&b){
-		_curState = 3;
-	}
-	
-	myDCEncoder::updateCurPos();
+  // update state info
+  _lastState = _curState;
+
+  // logic based on behavior of the enoder. See spec sheet
+  if (!a && !b) {
+    _curState = 0;
+  } else if (a && !b) {
+    _curState = 1;
+  } else if (a && b) {
+    _curState = 2;
+  } else if (!a && b) {
+    _curState = 3;
+  }
+
+  myDCEncoder::updateCurPos();
 }
 
 // splitting this from the updateCurState is useful bc:
 // at a minimum, can help increase readability
-// if the interrupt is found to be taking too long (hasn't yet), a flag can be set in updateCurState which will be handled by
-// updateCurPos. If the flag is true, run updateCurPos and set the flag to false
-void myDCEncoder::updateCurPos(){
+// if the interrupt is found to be taking too long (hasn't yet), a flag can be
+// set in updateCurState which will be handled by updateCurPos. If the flag is
+// true, run updateCurPos and set the flag to false
+void myDCEncoder::updateCurPos() {
 
-	// organize time data
-	static unsigned int lastTime = millis();
-	unsigned int curTime = millis();
-	unsigned int dt = curTime - lastTime;
-	lastTime = curTime;
+  // organize time data
+  static unsigned int lastTime = millis();
+  unsigned int curTime = millis();
+  unsigned int dt = curTime - lastTime;
+  lastTime = curTime;
 
-	// cw is 0, 1, 2, 3
-	switch (_curState){
-		case 0:
-		if (_lastState == 3){
-			_curPos ++;
-			_curOmega = 1/dt;
-		}
-		else if (_lastState == 1){
-			_curPos --;
-			_curOmega = -1/dt;
-		}
-		else if (_lastState == 0){
-			_curOmega = 0;
-		}
-		break;
+  // cw is 0, 1, 2, 3
+  switch (_curState) {
+  case 0:
+    if (_lastState == 3) {
+      _curPos++;
+      _curOmega = 1 / dt;
+    } else if (_lastState == 1) {
+      _curPos--;
+      _curOmega = -1 / dt;
+    } else if (_lastState == 0) {
+      _curOmega = 0;
+    }
+    break;
 
-		case 1:
-		if (_lastState == 0){
-			_curPos ++;
-			_curOmega = 1/dt;
-		}
-		else if (_lastState == 2){
-			_curPos --;
-			_curOmega = -1/dt;
-		}
-		else if (_lastState == 1){
-			_curOmega = 0;
-		}
-		break;
+  case 1:
+    if (_lastState == 0) {
+      _curPos++;
+      _curOmega = 1 / dt;
+    } else if (_lastState == 2) {
+      _curPos--;
+      _curOmega = -1 / dt;
+    } else if (_lastState == 1) {
+      _curOmega = 0;
+    }
+    break;
 
-		case 2:
-		if (_lastState == 1){
-			_curPos ++;
-			_curOmega = 1/dt;
-		}
-		else if (_lastState == 3){
-			_curPos --;
-			_curOmega = -1/dt;
-		}
-		else if (_lastState == 2){
-			_curOmega = 0;
-		}
-		break;
+  case 2:
+    if (_lastState == 1) {
+      _curPos++;
+      _curOmega = 1 / dt;
+    } else if (_lastState == 3) {
+      _curPos--;
+      _curOmega = -1 / dt;
+    } else if (_lastState == 2) {
+      _curOmega = 0;
+    }
+    break;
 
-		case 3:
-		if (_lastState == 2){
-			_curPos ++;
-			_curOmega = 1/dt;
-		}
-		else if (_lastState == 0){
-			_curPos --;
-			_curOmega = -1/dt;
-		}
-		else if (_lastState == 3){
-			_curOmega = 0;
-		}
-		break;
-	}
-
+  case 3:
+    if (_lastState == 2) {
+      _curPos++;
+      _curOmega = 1 / dt;
+    } else if (_lastState == 0) {
+      _curPos--;
+      _curOmega = -1 / dt;
+    } else if (_lastState == 3) {
+      _curOmega = 0;
+    }
+    break;
+  }
 }
+
+/////////////////////////////////////////////////
+/////////////// HELPER METHODS //////////////////
+/////////////////////////////////////////////////
+
+void myDCEncoder::ticksToDeg() {}
--- Project/Arduino/main/src/myDCEncoder/myDCEncoder.h
+++ Project/Arduino/main/src/myDCEncoder/myDCEncoder.h
@@ -3,28 +3,41 @@
 
 #include "Arduino.h"
 
-class myDCEncoder
-{
-  public:
-    myDCEncoder(int IN_1, int IN_2, int EN, int ch_a, int ch_b);
-    void init();
-    void driveMotor(int PWM);
-    void updateCurState();
-    void PID(int desPos);
-    void ticksToDeg();
-    int _curPos = 0; // in "ticks"
-    int _curOmega = 0; // in tick/ms
-    float _kp = 10.0;
-    float _ki = 0.05;
-    float _kd = 1.0;
-  private:
-    void updateCurPos();
-    int _curState = 0;
-    int _lastState = 0;
-    int _IN_1;
-    int _IN_2;
-    int _EN;
-    int _input_ch[2];
+class myDCEncoder {
+public:
+  myDCEncoder(int IN_1, int IN_2, int EN, int ch_a, int ch_b,
+              int hallEffectPin);
+  void init();
+  void driveMotor(int PWM);
+  void updateCurState();
+  void PID(int desPos);
+  void ticksToDeg();
+  void stopMotor();
+  void releaseMotor();
+  void calibrateMotor();
+  int _curPos = 0;   // in "ticks";
+  int _curOmega = 0; // in tick/ms;
+  float _kp = 10.0;
+  float _ki = 0.00005;
+  float _kd = 1.0;
+  int _EN;
+  int _IN_1;
+  int _IN_2;
+  int _hallEffectPin;
+  float _hallEffectReading;
+  float _hallEffectReadingG;
+
+private:
+  void updateCurPos();
+  int _curState = 0;
+  int _lastState = 0;
+  bool _calibrated = 0;
+  float _hallEffectVCC = 5.0;
+  float _zeroField = _hallEffectVCC / 2;
+  float _hallEffectSensitivity = .001;
+  float _hallEffectResolution = _hallEffectVCC / 1023.0;
+
+  int _input_ch[2];
 };
 
 #endif
